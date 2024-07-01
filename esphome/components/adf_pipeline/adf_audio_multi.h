a9cdff88230d8ed8a862269cc6977fc3a758ac9b
Jonah Butler <61718056+jonahnm@users.noreply.github.com>
2024-07-01 16:56:31 -0400

    Create adf_audio_multi.h

---
+++ esphome/components/adf_pipeline/adf_audio_multi.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifdef USE_ESP_IDF
+
+#include "adf_audio_element.h"
+
+namespace esphome {
+namespace esp_adf {
+
+class ADFPipelineProcessElement : public ADFPipelineElement {
+ public:
+  AudioPipelineElementType get_element_type() const { return AudioPipelineElementType::AUDIO_PIPELINE_PROCESS; }
+};
+
+class ADFMulti : public ADFPipelineProcessElement {
+ public:
+  const std::string get_name() override { return "Multi"; }
+
+ protected:
+  bool init_adf_elements_() override;
+  void on_settings_request(AudioPipelineSettingsRequest &request) override;
+
+  audio_element_handle_t sdk_resampler_;
+};
+
+}  // namespace esp_adf
+}  // namespace esphome
+#endif
